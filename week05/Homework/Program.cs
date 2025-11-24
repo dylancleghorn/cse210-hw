cc745e9fbe993f28efacfe323dc9e64a2fc4a269
Dylan Cleghorn <dylan.cleghorn@gmail.com>
2025-11-24 14:48:11 -0600

    playing with content

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,18 +5,18 @@ class Program
     static void Main(string[] args)
     {
         // Create a base "Assignment" object
-        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        Assignment a1 = new Assignment("Dylan Cleghorn", "Algebra");
         Console.WriteLine(a1.GetSummary());
         Console.WriteLine();
 
         // Now create the derived class assignments
-        MathAssignment a2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        MathAssignment a2 = new MathAssignment("Ingrid Cleghorn", "Calculus", "10", "1-20");
         Console.WriteLine(a2.GetSummary());
         Console.WriteLine(a2.GetHomeworkList());
         Console.WriteLine();
 
 
-        WritingAssignment a3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        WritingAssignment a3 = new WritingAssignment("Elijah Cleghorn", "Numbers", "The Number Two");
         Console.WriteLine(a3.GetSummary());
         Console.WriteLine(a3.GetWritingInformation());
         Console.WriteLine();
